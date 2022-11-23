21b96edd420cae38f3d5d557523051efd7304312
ratin17 <ratin.cse@gmail.com>
2022-11-24 02:54:20 +0600

    X long 3 problem

--- Xettabyte/X_Long_3/e_primeFactors.cpp
+++ Xettabyte/X_Long_3/e_primeFactors.cpp
@@ -8,7 +8,30 @@ using namespace std;
 int main(){
     ll n;
     while(cin>>n){
+        if(n==0)break;
+
+        ll m=abs(n);
+        vector<ll>v;
         
+        ll i;
+        for(i=2;i*i<=m;i++){
+            if(m%i==0){
+                v.push_back(i);
+                m/=i;
+                i=1;
+            }
+        }
+        if(m!=1)v.push_back(m);
+
+        sort(v.begin(),v.end());
+        cout<<n<<" =";
+        if(n<0)cout<<" -1";
+        ra(j,0,v.size()){
+            if(n>0 && j==0)cout<<" "<<v[j];
+            else cout<<" x "<<v[j];
+        }
+        cout<<endl;
+
     }
     
     
